2f0d52047e96d4de7fa972ff4a03930db666b82e
PetrovaNina <petrova1nina@gmail.com>
2021-03-18 13:15:55 +0300

    Projects description transferred to JS object

---
+++ script.js
@@ -0,0 +1,37 @@
+const projectsDescription = {
+    findCat: {
+        accomplishments: [
+            "Concept generation",
+            "Built with JavaScript",
+            "Creating and interaction with DOM elements dynamically through JS",
+            "Responsive designs",
+            "CSS animation"
+        ],
+        "View site": "https://petrovanina.github.io/FindCatGame",
+    },
+    educational: {
+        accomplishments: [
+            "Concept generation",
+            "HTML layouts encoding and styling with CSS Flex and Grid",
+            "Fully responsive design including survey form and table",
+            "Hover and click actions"
+        ],
+        "View site": "https://petrovanina.github.io/educational-organization-website",
+    },
+    showcase: {
+        accomplishments: [
+            "HTML layouts encoding and styling with CSS Flex and Grid",
+            "Adaptive page layout encoding",
+            "Hover and click actions"
+        ],
+        "View site": "https://petrovanina.github.io/Online-Store-Showcase",
+    },
+    techDoc: {
+        accomplishments: [
+            "HTML layouts encoding",
+            "Responsive design",
+            "Page sections navigate by clicking the corresponding navbar element"
+        ],
+        "View site": "https://petrovanina.github.io/TechDoc-Page",
+    },
+}
