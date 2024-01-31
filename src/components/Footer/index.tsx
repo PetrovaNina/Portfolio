feb8cd5cb69f8a490e5162b6bbf74e2f53987101
Nina Petrova <git config --petrova1nina@gmail.com>
2024-01-31 14:21:21 +0200

    feat: footer date

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className={s.Footer}>
       <div className={s.container}>
-        <p>Nina Petrova, 2021</p>
+        <p>Nina Petrova, 2024</p>
       </div>
     </footer>
   );
